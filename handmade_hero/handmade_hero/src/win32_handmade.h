22ed98be7a6fc521fb8ab52c11a3f5237f2b61af
vosure <vosure1@gmail.com>
2019-03-04 18:27:22 +0300

    changed project structure

---
+++ handmade_hero/handmade_hero/src/win32_handmade.h
@@ -0,0 +1,28 @@
+#pragma once
+struct win32_offscreen_buffer
+{
+	BITMAPINFO Info;
+	void *Memory;
+	int Width;
+	int Height;
+	int Pitch;
+	int BytesPerPixel;
+};
+
+struct win32_window_dimension
+{
+	int Width;
+	int Height;
+};
+
+struct win32_sound_output
+{
+	int32 SamplesPerSecond;
+	int32 ToneHz;
+	int32 ToneVolume;
+	uint32 RunningSampleIndex;
+	int32 BytesPerSample;
+	int32 SecondaryBufferSize;
+	real32 tSine;
+	int32 LatencySampleCount;
+};
